33238d451e040355511fac16afef198c3dfbd50d
Madhu Duggirala <madhuduggirala@madhus-mbp.lan>
2023-09-17 23:07:04 -0400

    added

---
+++ Selenium/day14iframe.java
@@ -0,0 +1,51 @@
+package Selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class day14iframe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// WebDriver | IFrame--> title
+		// #frame
+		// #div-main-nav > div > ul > li.active > a
+		
+		
+		
+		ChromeDriver driver = new ChromeDriver();
+		// visiting the url
+		driver.get("http://www.webdriveruniversity.com/IFrame/index.html");
+		
+		// iframe
+	//	WebElement ele = driver.findElement(By.cssSelector("#frame"));
+	//	driver.switchTo().frame(ele);
+		
+		// switch to iframe by index
+		
+		driver.switchTo().frame(0);
+		WebElement ele2 = driver.findElement(By.cssSelector(" #div-main-nav > div > ul > li.active > a"));
+		System.out.println(ele2.isDisplayed());
+		
+		// switching back to main content
+		driver.switchTo().defaultContent();
+		// finding the element in main content
+		driver.findElement(By.cssSelector("#nav-title")).isDisplayed();
+		
+		// validating the title of main content
+		if(driver.getTitle().equals("WebDriver | IFrame")) {
+			System.out.println("test case pass");
+		}
+		else {
+			System.out.println("test case fails");
+		}
+		
+		driver.close();
+		
+		
+		
+	}
+
+}
